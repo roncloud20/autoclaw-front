7005f2f275be20aad87586672a512ea9b1959aee
Wilson Emmanuel <wilsonedikanabasi@gmail.com>
2026-04-07 09:47:00 +0100

    contact us

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Contact from "./pages/contact";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       
       </BrowserRouter>
